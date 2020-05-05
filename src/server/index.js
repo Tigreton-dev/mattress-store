2a5e491d914cc2d4fd556ee546a4369e6ec53e37
javier <javier.san.mail@gmail.com>
2020-05-05 16:39:02 +0200

    setUp mongo DB and mongoose for data store

--- src/server/index.js
+++ src/server/index.js
@@ -2,9 +2,12 @@ const express = require('express');
 
 const app = express();
 
+require('./startup/db')();
+require('./startup/config')();
+
 const port = process.env.PORT || 3000;
 const server = app.listen(port, () =>
-    console.log(`Listening on port ${port}...`);
+    console.log(`Listening on port ${port}...`)
 );
 
 module.exports = server;
---
+++ src/server/startUp/db.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+const config = require('config');
+
+module.exports = function () {
+    const db = config.get('db');
+    mongoose
+        .connect(db, { useCreateIndex: true, useNewUrlParser: true })
+        .then(() => console.log('conected to MongoDB'));
+};
