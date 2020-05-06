134c2389231146ef918a0ed1d017b15998c4ecd8
javier <javier.san.mail@gmail.com>
2020-05-06 10:00:08 +0200

    added home page

---
+++ src/app/pages/Home.js
@@ -0,0 +1,19 @@
+import React, { useReducer, useEffect } from 'react';
+
+
+const Home = () => {
+    return (
+        <div className="Home-container">
+            <h1>Week offer</h1>
+            <div className="featured-offertWeek">
+                <img
+                    src={"https://img4882.weyesimg.com/uploads/www.springmattressfactory.com/images/153985301300120.png"}
+                    alt={name}
+                />
+            </div>
+
+        </div>
+    );
+};
+
+export default Home;
