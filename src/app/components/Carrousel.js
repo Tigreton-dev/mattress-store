be72735d915cddf93075bfbf73a8fa140b3dd9cd
javier <javier.san.mail@gmail.com>
2020-05-10 12:13:59 +0200

    refactoring carrousel adding autoplay functionallity

--- src/app/components/Carrousel.js
+++ src/app/components/Carrousel.js
@@ -1,20 +1,33 @@
 import React, { useState } from 'react';
-import { Carousel, CarouselControl, CarouselItem, CarouselCaption } from 'reactstrap';
+import { Carousel, CarouselControl, CarouselItem, CarouselCaption, CarouselIndicators } from 'reactstrap';
 import CarrouselItemComponent from './CarrouselItemComponent';
 
 const Carrousel = props => {
     const { products } = props;
+
     const [activeIndex, setActiveIndex] = useState(0);
     const [animating, setAnimating] = useState(false);
 
+    const next = () => {
+        if (animating) return;
+        const nextIndex = activeIndex === products.length - 1 ? 0 : activeIndex + 1;
+        setActiveIndex(nextIndex);
+    }
+
+    const previous = () => {
+        if (animating) return;
+        const nextIndex = activeIndex === 0 ? products.length - 1 : activeIndex - 1;
+        setActiveIndex(nextIndex);
+    }
+
+    const goToIndex = (newIndex) => {
+        if (animating) return;
+        setActiveIndex(newIndex);
+    }
+
     const slides = products.map((product) => {
         return (
-            <CarouselItem
-                className="custom-tag"
-                tag="div"
-                onExiting={() => setAnimating(true)}
-                onExited={() => setAnimating(true)}
-            >
+            <CarouselItem className="custom-tag" tag="div" onExiting={() => setAnimating(true)} onExited={() => setAnimating(false)}>
                 <CarrouselItemComponent product={product} />
                 <CarouselCaption className="text-danger" />
             </CarouselItem>
@@ -23,10 +36,11 @@ const Carrousel = props => {
 
     return (
         <div>
-            <Carousel activeIndex={activeIndex} >
+            <Carousel activeIndex={activeIndex} next={next} previous={previous} ride="carousel" interval="6000" >
+                <CarouselIndicators items={products} activeIndex={activeIndex} onClickHandler={goToIndex} />
                 {slides}
-                <CarouselControl />
-                <CarouselControl />
+                <CarouselControl direction="prev" directionText="Previous" onClickHandler={previous} className="carrousel-control" />
+                <CarouselControl direction="next" directionText="Next" onClickHandler={next} className="carrousel-control" />
             </Carousel>
         </div>
     );
